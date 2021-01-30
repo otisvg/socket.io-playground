562e62b3216a0eea4f8260898f4b8d7002a0bf9f
Otis <otisvickersgraver@gmail.com>
2021-01-30 23:30:46 +0000

    initialize socket.io client

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import http from 'http';
 import path from 'path';
+import io from 'socket.io';
+
+
+
+imort 
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
@@ -9,6 +14,10 @@ const HTTP = http.createServer(app);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 HTTP.listen(3000, () => {
